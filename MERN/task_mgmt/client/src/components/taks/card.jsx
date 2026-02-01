6947047626064f7a6f563fc94937770cc6fb6398
Abinash Regmi <abinashregmi01234@gmail.com>
2026-02-01 13:52:51 +0545

    here are the changes to cards.jsx: aa

--- MERN/task_mgmt/client/src/components/taks/card.jsx
+++ MERN/task_mgmt/client/src/components/taks/card.jsx
@@ -8,19 +8,23 @@ import { MdEdit } from "react-icons/md";
 
 export const Card = () => {
   return (
-    <div>
-      <div>
-        <p>Task</p>
-        <IoPinSharp/>
-        
+    <div className='border border-gray-300 py-3 px-4 rounded-md shadow'>
+      <div className='flex justify-between'>
+        <p className='text-lg font-bold text-gray-800'>Task</p>
+        <IoPinSharp size={22} className='text-gray-600'/>
       </div>
+    
       <div>
-      <p>Task Title</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quo architecto quidem dignissimos. Perferendis, voluptatibus? Pariatur, minima reiciendis porro qui voluptatibus quod laborum fugiat inventore, nihil aperiam et cumque incidunt.</p>
+        <p className='bg-green-500 w-25 rounded-md text-white font-bold text-sm py-0.5'>High</p>
+      </div>
 
-      <div>
-        <MdEdit/>
-        <BiSolidTrash/>
+      <div className='flex flex-col gap-1'>
+      <p className='text-{18px} font-semibold text-gray-700 line-clamp-1'>Task Title</p>
+      <p className='text-{14px} text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quo architecto quidem dignissimos. Perferendis, voluptatibus? Pariatur, minima reiciendis porro qui voluptatibus quod laborum fugiat inventore, nihil aperiam et cumque incidunt.</p>
+
+      <div className='flex justify-end gap-2 mt-1'>
+        <MdEdit title="Edit Task" size={20} className='text-blue-500'/>
+        <BiSolidTrash title="Delete Task" size={16} className='text-red-500'/>
 
 
 
