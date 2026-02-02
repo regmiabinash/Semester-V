70c9258ab2df07ebb18f8462d7d2af374d754e4c
Abinash Regmi <abinashregmi01234@gmail.com>
2026-02-02 12:43:56 +0545

    ok

--- MERN/task_mgmt/client/src/components/forms/task.form.jsx
+++ MERN/task_mgmt/client/src/components/forms/task.form.jsx
@@ -20,8 +20,38 @@ const AddEditTask = () =>{
                     <input 
                     name="title" 
                     id="title" 
-                    className="py-2.5 px-2 bg-slate-50 border border-blue-200 text-lg text-slate-950"/>
+                    className="py-2.5 px-2 bg-slate-50 border border-blue-200 text-lg text-slate-950 rounded-md focus:outline-blue-400"/>
                 
+                </div>
+                <div className="flex flex-col gap-1">
+                    <div className="flex">
+                    <label
+                    htmlFor="content"
+                    className="text-lg"
+                    placeholder="SM assignment"
+                    id="title">
+                        Content
+                    </label>
+                    
+                    <FaAsterisk size={12} className="text-red-500"/></div>
+                    <textarea 
+                    name="content" 
+                    id="content" 
+                    placeholder="Task description"
+                    className="py-2.5 px-2 bg-slate-50 border border-blue-200 text-lg text-slate-950 rounded-md focus:outline-blue-400"/>
+                
+                </div>
+                <div className="flex flex-col">
+                    <label htmlFor="priority" className="py-2.5 text-lg font-bold border border-blue-200 focus:outline-blue-400">Priority</label>
+                    <select 
+                    defaultValue={"low"}
+                    name="priority"
+                    id="priority"
+                    className="py-2.5 px-2 rounded-md text-lg font-bold border border-blue-200 focus:outline-blue-400">
+                        <option value={'high'}>High</option>
+                        <option value={'medium'}>Medium</option>
+                        <option value={'low'}>Low</option>
+                    </select>
                 </div>
             </form>
         </div>
